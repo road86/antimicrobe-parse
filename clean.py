fe2c343ddced974d7012e65528b3e6dbdb7b237f
M Kunde <mikolaj.kundegorski@gmail.com>
2022-08-03 16:06:00 +0600

    cleaning antbiot and pathogen data

---
+++ clean.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import re
+
+ast_data = pd.read_csv('ast_data.csv')
+
+#clean all starting with "growth of.."
+growth = re.compile('growth.*?Of',re.IGNORECASE)
+ast_data['pathogen'] = ast_data['pathogen'].astype(str).apply(lambda x: re.sub(growth,'',x))
+
+#general cleaning of post and prefixes and leading spaces
+ast_data['pathogen'] = ast_data['pathogen'].apply(lambda x: x.removesuffix(' l').rstrip('.').removesuffix('spp').lstrip().rstrip().lower().replace('. ','.').replace('.','. '))
+
+ast_data['pathogen'] = ast_data['pathogen'].astype(str).apply(lambda x: re.sub(" +", " ", x)) # remove multiple spaces
+
+#order alphabe
+las = sorted(list(ast_data['pathogen'].unique()))
+pd.Series(las).to_csv('chevpat.csv')
+
+#####
+## Antibiotics
+####
+
+
+ast_data['antibiotic'] = ast_data['antibiotic'].astype(str).apply(lambda x: re.sub(" +", " ", x)) # remove multiple spaces
+
+ast_data['antibiotic'] = ast_data['antibiotic'].apply(lambda x: x.replace(' /','/').replace('/ ','/').lstrip().rstrip().lower())
+
+las = sorted(list(ast_data['antibiotic'].unique()))
+pd.Series(las).to_csv('chevantbio.csv')
+
+
+
+#####
+## Specimen
+####
+
+las = sorted(list(ast_data['specimen'].unique()))
+pd.Series(las).to_csv('chev_specimen.csv')
