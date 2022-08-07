fe7b5d516ea2ce1b4ecf7cb7bea1264cf7ccaf71
M Kunde <mikolaj.kundegorski@gmail.com>
2022-08-07 10:07:06 +0600

    using lookup tables

--- clean.py
+++ clean.py
@@ -16,6 +16,9 @@ ast_data['pathogen'] = ast_data['pathogen'].astype(str).apply(lambda x: re.sub("
 las_pat = sorted(list(ast_data['pathogen'].unique()))
 pd.Series(las_pat).to_csv('chevpat.csv')
 
+
+
+
 #####
 ## Antibiotics
 ####
@@ -23,10 +26,20 @@ pd.Series(las_pat).to_csv('chevpat.csv')
 
 ast_data['antibiotic'] = ast_data['antibiotic'].astype(str).apply(lambda x: re.sub(" +", " ", x)) # remove multiple spaces
 
-ast_data['antibiotic'] = ast_data['antibiotic'].apply(lambda x: x.replace(' /','/').replace('/ ','/').lstrip().rstrip().lower().lstrip('/').rstrip('/'))
+ast_data['antibiotic'] = ast_data['antibiotic'].apply(lambda x: x.replace(' /','/').replace('/ ','/').lstrip().rstrip().lower().lstrip('/').rstrip('/').lstrip('('))
 
 las_anti = sorted(list(ast_data['antibiotic'].unique()))
 pd.Series(las_anti).to_csv('chevantbio.csv')
+
+
+###
+##R S I
+#####
+
+ast_data['result'] = ast_data['result'].astype(str).apply(lambda x: x.lstrip().rstrip().rstrip('/').rstrip('*').lstrip('*').upper()) # remove multiple spaces
+
+las_res = sorted(list(ast_data['result'].unique()))
+pd.Series(las_res).to_csv('chevres.csv')
 
 
 #####
@@ -35,5 +48,42 @@ pd.Series(las_anti).to_csv('chevantbio.csv')
 
 las_spec = sorted(list(ast_data['specimen'].unique()))
 pd.Series(las_spec).to_csv('chev_specimen.csv')
+
+ast_data.to_csv('outdata/ast_data_chevron_to_clean.csv')
+
+#########
+#######
+#### Use lookup table to fix all the different spellings
+##
+#
+
+
+lookup_table = pd.read_excel('../input_data/chevron-lookup-tables.xlsx',sheet_name=['pathogen','antibiotic','result','specimen'])
+
+pathogen_rep = lookup_table['pathogen'].set_index('spelling')['correct'].dropna().apply(lambda x: x.lower()).to_dict()
+missing_pathogen = ast_data[~ast_data['pathogen'].isin(pathogen_rep.keys())]
+ast_data = ast_data[ast_data['pathogen'].isin(pathogen_rep.keys())]
+ast_data['pathogen'] = ast_data['pathogen'].replace(pathogen_rep)
+
+
+antibiotic_rep = lookup_table['antibiotic'].set_index('spelling')['correct'].dropna().apply(lambda x: x.lower()).to_dict()
+missing_antibiotic = ast_data[~ast_data['antibiotic'].isin(antibiotic_rep.keys())]
+ast_data = ast_data[ast_data['antibiotic'].isin(antibiotic_rep.keys())]
+ast_data['antibiotic'] = ast_data['antibiotic'].replace(antibiotic_rep)
+
+
+result_rep = lookup_table['result'].set_index('spelling')['correct'].dropna().apply(lambda x: x.lower()).to_dict()
+missing_result = ast_data[~ast_data['result'].isin(result_rep.keys())]
+ast_data = ast_data[ast_data['result'].isin(result_rep.keys())]
+ast_data['result'] = ast_data['result'].replace(result_rep)
+
+
+specimen_rep = lookup_table['specimen'].set_index('spelling')['correct'].dropna().apply(lambda x: x.lower()).to_dict()
+missing_specimen = ast_data[~ast_data['specimen'].isin(specimen_rep.keys())]
+ast_data = ast_data[ast_data['specimen'].isin(specimen_rep.keys())]
+ast_data['specimen'] = ast_data['specimen'].replace(specimen_rep)
+
+specimen_category_rep = lookup_table['specimen'].set_index('correct')['specimen_category'].dropna().apply(lambda x: x.lower()).to_dict()
+ast_data['specimen_category'] = ast_data['specimen'].replace(specimen_category_rep)
 
 ast_data.to_csv('outdata/ast_data_chevron_clean.csv')
