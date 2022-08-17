32bb5da13463c7bb45f330a6c8abedc1d2053e29
wali <walizmn@gmai.com>
2022-08-17 10:54:15 +0600

    rearranged and edited the script a bit, as it was not working on my system.

--- parse_adhumic.py
+++ parse_adhumic.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 import os
 
-adhunik_data = os.path.join('..','input_data', 'unsorted', 'Raw AST data_ready to use', 'Uttara Adhumic Medical college, Dhaka_2021.xlsx')
+adhunik_data = os.path.join('..','input_data', 'unsorted', 'AST data_ready to use', 'Uttara Adhumic Medical college, Dhaka_2021.xlsx')
 
 df = pd.read_excel(adhunik_data, header=[0,1,2]) #first three lines a header
 corrected_cols = [x[1] for x in df.columns[0:11]] + [x[2] for x in df.columns[11:]]
@@ -28,6 +28,7 @@ for iii, rrr in df.iterrows():
         if not anew_df['pathogen'] in ['No Growth', 'Mixed Growth', 'Due']:
              megagigalist.append(anew_df)
 
+ast_data = pd.DataFrame.from_records(megagigalist)
 ast_data =  ast_data.dropna(subset=['sensitivity']) #remove many datapoints for antibiotics not tested for each sample
 
 outputloc = "outdata"
