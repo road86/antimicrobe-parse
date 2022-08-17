05c3cf7c555db3fa6818e712f56a3ea2c7aa254e
M Kunde <mikolaj.kundegorski@gmail.com>
2022-08-17 12:31:36 +0200

    better information when running script

--- run.sh
+++ run.sh
@@ -1,9 +1,18 @@
 set -e
+echo 'python parse.py'
 python parse.py
+echo 'python parse_square.py'
 python parse_square.py
+echo 'python parse_adhumic.py'
 python parse_adhumic.py
+echo 'python parse_birdem.py'
 python parse_birdem.py
+echo 'python parse.py'
 python parse.py
+echo 'python clean.py'
 python clean.py
+echo 'R < amr_preprocess.r --no-save'
 R < amr_preprocess.r --no-save
+echo 'python prepare_matrix.py'
 python prepare_matrix.py
+echo 'Done!'
--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,7 @@
 set -e
+echo 'AMR_TEST=test python clean.py'
 AMR_TEST=test python clean.py
+echo 'R < amr_preprocess.r --no-save'
 R < amr_preprocess.r --no-save
+echo 'python prepare_matrix.py'
 python prepare_matrix.py
