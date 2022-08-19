9a639a47f7d75005f78ad23521d4d471be001d7d
M Kunde <mikolaj.kundegorski@gmail.com>
2022-08-19 16:35:27 +0200

    antibiogram for locations and providers

--- amr_preprocess.py
+++ amr_preprocess.py
@@ -1,19 +1,44 @@
 import pandas as pd
 import os
 
-cleaneddata = pd.read_csv(os.path.join("outdata","ast_data_clean.csv"))
+indata = pd.read_csv(os.path.join("outdata","ast_data_clean.csv"))
 
-prepdata = cleaneddata.groupby(['specimen_category', 'pathogen','antibiotic','sensitivity'])['amr_uuid'].count().reset_index()
-prepdata2 = pd.pivot(prepdata, index=['specimen_category','pathogen','antibiotic'], columns='sensitivity', values='amr_uuid').reset_index()
+providers = list(indata['provider'].unique())
+locations = list(indata['location'].unique())
 
-prepdata2.columns=['specimen_category', 'pathogen', 'antibiotic', 'count_i', 'count_r', 'count_s']
-prepdata2 = prepdata2.fillna(0)
-prepdata2['total'] = prepdata2[['count_r','count_s','count_i']].sum(axis=1)
-prepdata2['sensitivity'] = prepdata2.apply(lambda x: x['count_s']/x['total'],axis=1)
+masks = dict()
 
-prepdata2.to_csv('pre-processed.csv')
+for prov in providers:
+    masks[(prov,'bgd')] = (indata['provider']==prov)
+
+for loc in locations:
+    masks[('all',loc)] = (indata['location']==loc)
+
+masks[('all','bgd')] = pd.Series(True,indata.index)
+
+list_of_versions = []
+
+for maskey in masks.keys():
+    cleaneddata = indata[masks[maskey]]
+    prov = maskey[0]
+    loc = maskey[1]
+    lv = {'provider':prov, 'location':loc}
+    list_of_versions.append(lv)
+
+    prepdata = cleaneddata.groupby(['specimen_category', 'pathogen','antibiotic','sensitivity'])['amr_uuid'].count().reset_index()
+    prepdata2 = pd.pivot(prepdata, index=['specimen_category','pathogen','antibiotic'], columns='sensitivity', values='amr_uuid').reset_index()
+
+    prepdata2.columns = list(prepdata2.columns[:3]) + [ 'count_' + x for x in prepdata2.columns[3:]]
+
+    prepdata2 = prepdata2.fillna(0)
+    prepdata2['total'] = prepdata2[list(prepdata2.columns[3:])].sum(axis=1)
+    prepdata2['sensitivity'] = prepdata2.apply(lambda x: x['count_s']/x['total'],axis=1)
+
+    prepdata2.to_csv(os.path.join('outdata',f'pre-processed-{prov}-{loc}.csv'))
 
 
-prepnoiso= cleaneddata.groupby(['specimen_category', 'pathogen'])['amr_uuid'].nunique().reset_index()
-prepnoiso.columns = ['specimen_category', 'pathogen', 'number_of_isolates']
-prepnoiso.to_csv('number_of_isolates.csv')
+    prepnoiso= cleaneddata.groupby(['specimen_category', 'pathogen'])['amr_uuid'].nunique().reset_index()
+    prepnoiso.columns = ['specimen_category', 'pathogen', 'number_of_isolates']
+    prepnoiso.to_csv(os.path.join('outdata',f'number-of-isolates-{prov}-{loc}.csv'))
+
+pd.DataFrame.from_records(list_of_versions).to_csv(os.path.join('outdata','list_of_versions.csv'))
--- prepare_matrix.py
+++ prepare_matrix.py
@@ -10,119 +10,129 @@ def mper(val):
 #threshold of number of isolates tested for given antibiotic out of total number of isolates of this bacteria. If it is below the threshold we are not showing a result
 thresh=0.02
 
-prepo = pd.read_csv('pre-processed.csv')
-noi = pd.read_csv('number_of_isolates.csv')
+list_of_versions = pd.read_csv(os.path.join('outdata','list_of_versions.csv'))
 
-# rawsummary = pd.merge(prepo,noi, on = ['specimen_category','pathogen'])
-# easier to use to dfs for noi and rest of the data
-rawsummary = prepo[['specimen_category', 'pathogen', 'antibiotic', 'total', 'sensitivity']]
+for _, ver in list_of_versions.iterrows():
+    prov = ver['provider']
+    loc = ver['location']
 
+    prepo = pd.read_csv(os.path.join('outdata',f'pre-processed-{prov}-{loc}.csv'))
+    noi = pd.read_csv(os.path.join('outdata',f'number-of-isolates-{prov}-{loc}.csv'))
 
-#  ######
-#    ##
-#    ##
-#    ##
-#    ##
-#  ######
-#
-#  Calculate antibiogram
-#
+    # rawsummary = pd.merge(prepo,noi, on = ['specimen_category','pathogen'])
+    # easier to use to dfs for noi and rest of the data
+    rawsummary = prepo[['specimen_category', 'pathogen', 'antibiotic', 'total', 'sensitivity']]
 
 
-with pd.ExcelWriter("amb.xlsx", engine="openpyxl") as writer:
-    for specimen_type in prepo['specimen_category'].unique():
-        print(specimen_type)
-        df = rawsummary[rawsummary['specimen_category']==specimen_type]
-        df_n = noi[noi['specimen_category']==specimen_type]
-        df_n = df_n.sort_values(by=['number_of_isolates'], ascending = False)
-        df_n = df_n.set_index('pathogen')
+    #  ######
+    #    ##
+    #    ##
+    #    ##
+    #    ##
+    #  ######
+    #
+    #  Calculate antibiogram
+    #
+    print("################################")
+    print("################################")
+    print(f"amb-{prov}-{loc}.xlsx")
+    print("################################")
+    print("################################")
 
-        amb_matrix_perc = pd.pivot(df, index='antibiotic', columns='pathogen', values='sensitivity')
-        amb_matrix_tot = pd.pivot(df, index='antibiotic', columns='pathogen', values='total')
+    with pd.ExcelWriter(os.path.join("outdata",f"amb-{prov}-{loc}.xlsx"), engine="openpyxl") as writer:
+        for specimen_type in prepo['specimen_category'].unique():
+            print(specimen_type)
+            df = rawsummary[rawsummary['specimen_category']==specimen_type]
+            df_n = noi[noi['specimen_category']==specimen_type]
+            df_n = df_n.sort_values(by=['number_of_isolates'], ascending = False)
+            df_n = df_n.set_index('pathogen')
 
-        new_row = dict()
-        full_amb_list = []
+            amb_matrix_perc = pd.pivot(df, index='antibiotic', columns='pathogen', values='sensitivity')
+            amb_matrix_tot = pd.pivot(df, index='antibiotic', columns='pathogen', values='total')
 
-        total_n_iso = df_n['number_of_isolates'].sum()
-        new_row[('total','--')] = total_n_iso
-
-        for pat, niso in df_n.iterrows():
-            new_row[(pat,'n')] = niso['number_of_isolates']
-            new_row[(pat,'%')] = mper(niso['number_of_isolates'] / total_n_iso)
-
-
-        new_row[('overall sensitivity','%')] = '--'
-        full_amb_list.append(new_row)
-        tots_row = new_row
-
-        for (antibio, aperc), (_, atot) in zip(amb_matrix_perc.iterrows(), amb_matrix_tot.iterrows()):
             new_row = dict()
-            new_row[('total','--')] = atot.sum()
-            oversense = 0
-            for pat, _ in df_n.iterrows():
-                if np.isnan(atot[pat]):
-                    new_row[(pat,'n')] = 0
-                    new_row[(pat,'%')] = '--'
-                else:
-                    #if number of isolates tested on this antibiotic i less than some percentage  of total number of isolates we will not report sensitivity
-                    new_row[(pat,'n')] = atot[pat]
-                    if (atot[pat] / tots_row[(pat,'n')]) < thresh:
+            full_amb_list = []
+
+            total_n_iso = df_n['number_of_isolates'].sum()
+            new_row[('total','--')] = total_n_iso
+
+            for pat, niso in df_n.iterrows():
+                new_row[(pat,'n')] = niso['number_of_isolates']
+                new_row[(pat,'%')] = mper(niso['number_of_isolates'] / total_n_iso)
+
+
+            new_row[('overall sensitivity','%')] = '--'
+            full_amb_list.append(new_row)
+            tots_row = new_row
+
+            for (antibio, aperc), (_, atot) in zip(amb_matrix_perc.iterrows(), amb_matrix_tot.iterrows()):
+                new_row = dict()
+                new_row[('total','--')] = atot.sum()
+                oversense = 0
+                for pat, _ in df_n.iterrows():
+                    if np.isnan(atot[pat]):
+                        new_row[(pat,'n')] = 0
                         new_row[(pat,'%')] = '--'
                     else:
-                        new_row[(pat,'%')] = mper(aperc[pat])
-                    n_times_per = atot[pat] * aperc[pat]
-                    oversense = oversense + n_times_per
-            new_row['overall sensitivity','%'] = mper(oversense/atot.sum())
-            full_amb_list.append(new_row)
+                        #if number of isolates tested on this antibiotic i less than some percentage  of total number of isolates we will not report sensitivity
+                        new_row[(pat,'n')] = atot[pat]
+                        if (atot[pat] / tots_row[(pat,'n')]) < thresh:
+                            new_row[(pat,'%')] = '--'
+                        else:
+                            new_row[(pat,'%')] = mper(aperc[pat])
+                        n_times_per = atot[pat] * aperc[pat]
+                        oversense = oversense + n_times_per
+                new_row['overall sensitivity','%'] = mper(oversense/atot.sum())
+                full_amb_list.append(new_row)
 
-        full_amb_index = ['Total number of isolates'] + list(amb_matrix_perc.index)
+            full_amb_index = ['Total number of isolates'] + list(amb_matrix_perc.index)
 
-        full_amb = pd.DataFrame.from_records(full_amb_list,index=full_amb_index)
+            full_amb = pd.DataFrame.from_records(full_amb_list,index=full_amb_index)
 
-        new_columns = pd.MultiIndex.from_tuples(full_amb.columns, names=['Pathogen', 'number of isolates and sensitivity'])
-        full_amb.columns = new_columns
-        full_amb.to_excel(writer,sheet_name=specimen_type)
-        # Set backgrund colors depending on cell values
-        worksheet = writer.sheets[specimen_type]
+            new_columns = pd.MultiIndex.from_tuples(full_amb.columns, names=['Pathogen', 'number of isolates and sensitivity'])
+            full_amb.columns = new_columns
+            full_amb.to_excel(writer,sheet_name=specimen_type)
+            # Set backgrund colors depending on cell values
+            worksheet = writer.sheets[specimen_type]
 
 
-#  ######   ######  ######
-#    ##       ##      ##
-#    ##       ##      ##
-#    ##       ##      ##
-#    ##       ##      ##
-#  ######   ######  ######
-#
-#  formatting
-#
-        heading_size = 3 #I'm not setting it up anywhere it is for some reason longer than I'd want
+    #  ######   ######  ######
+    #    ##       ##      ##
+    #    ##       ##      ##
+    #    ##       ##      ##
+    #    ##       ##      ##
+    #  ######   ######  ######
+    #
+    #  formatting
+    #
+            heading_size = 3 #I'm not setting it up anywhere it is for some reason longer than I'd want
 
-        worksheet.row_dimensions[1].height = 150
+            worksheet.row_dimensions[1].height = 150
 
-        worksheet.column_dimensions[worksheet.cell(row=3,column=1).column_letter].width = 40
-        for ccc in range(2,len(full_amb.columns)+2):
-            if ccc%2:
-                worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
-            else:
-                worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
-            worksheet.cell(row=1,column=ccc).alignment = Alignment(textRotation=70)
-            for rrr in range(heading_size+1,heading_size+1+len(full_amb)):
-                worksheet.cell(row=rrr,column=ccc).alignment = Alignment(vertical='center')
-                worksheet.cell(row=rrr,column=ccc).alignment = Alignment(horizontal='center')
-
-        # worksheet.column_dimensions[worksheet.cell(row=3,column=len(full_amb.columns)+1).column_letter].width = 20
-        # worksheet.column_dimensions[worksheet.cell(row=3,column=len(full_amb.columns)).column_letter].width = 20
-
-        #starting after first entry which is pathogen wide totals
-        for rrr in range(heading_size+2,heading_size+1+len(full_amb)):
-            for ccc in list(range(4,len(full_amb.columns)+1,2))+[len(full_amb.columns)+1]:
-                value_str = worksheet.cell(row=rrr,column=ccc).value
-                if value_str == '--':
-                    worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('ededed'))
+            worksheet.column_dimensions[worksheet.cell(row=3,column=1).column_letter].width = 40
+            for ccc in range(2,len(full_amb.columns)+2):
+                if ccc%2:
+                    worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
                 else:
-                    value = int(value_str.rstrip('%'))
-                    worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('d62d20' if value < 50 else 'ed6f00' if value < 75 else 'eee600' if value < 90 else 'a0e040'))
+                    worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
+                worksheet.cell(row=1,column=ccc).alignment = Alignment(textRotation=70)
+                for rrr in range(heading_size+1,heading_size+1+len(full_amb)):
+                    worksheet.cell(row=rrr,column=ccc).alignment = Alignment(vertical='center')
+                    worksheet.cell(row=rrr,column=ccc).alignment = Alignment(horizontal='center')
 
-        for ccc in range(1,len(full_amb.columns)+2):
-            worksheet.cell(row=4,column=ccc).fill = PatternFill("solid", start_color='6495ed')
-        worksheet.delete_rows(3,1)
+            # worksheet.column_dimensions[worksheet.cell(row=3,column=len(full_amb.columns)+1).column_letter].width = 20
+            # worksheet.column_dimensions[worksheet.cell(row=3,column=len(full_amb.columns)).column_letter].width = 20
+
+            #starting after first entry which is pathogen wide totals
+            for rrr in range(heading_size+2,heading_size+1+len(full_amb)):
+                for ccc in list(range(4,len(full_amb.columns)+1,2))+[len(full_amb.columns)+1]:
+                    value_str = worksheet.cell(row=rrr,column=ccc).value
+                    if value_str == '--':
+                        worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('ededed'))
+                    else:
+                        value = int(value_str.rstrip('%'))
+                        worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('d62d20' if value < 50 else 'ed6f00' if value < 75 else 'eee600' if value < 90 else 'a0e040'))
+
+            for ccc in range(1,len(full_amb.columns)+2):
+                worksheet.cell(row=4,column=ccc).fill = PatternFill("solid", start_color='6495ed')
+            worksheet.delete_rows(3,1)
