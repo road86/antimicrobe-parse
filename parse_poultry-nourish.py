3be4ce39fb52bdd85210c290c27e124d9e5b435e
wali <walizmn@gmai.com>
2022-08-22 20:55:28 +0600

    edited the script a bit, but still need help with calculation

--- parse_poultry-nourish.py
+++ parse_poultry-nourish.py
@@ -17,11 +17,49 @@ df2.to_csv(f'{datacheck}/nourish2.csv')
 df3.to_csv(f'{datacheck}/nourish3.csv')
 ##################
 
-ast_poultry = pd.melt(df1,id_vars=df1.columns[0:6], value_vars=df1.columns[6:-2],var_name='antibiotic', value_name='sensitivity')
+#for each antibiotic result there is a columns
+abio_columns_1 = range(3, len(df1.columns))
+abio_columns_2 = range(3, len(df2.columns))
+abio_columns_3 = range(3, len(df3.columns))
 
-ast_poultry = ast_poultry.dropna(subset=['sensitivity']) #remove many datapoints for antibiotics not tested for each sample
-ast_poultry = ast_poultry.reset_index()
-ast_poultry = ast_poultry[['index','Organism', 'Isolates_number' ,'antibiotic','sensitivity']]
+megagigalist = []
 
-ast_poultry.columns = ['pathogen', 'isolates' ,'antibiotic', 'sensitivity']
+# for E. coli
+for iii, rrr in df1.iterrows():
+    for abioind1 in abio_columns_1:
+        new_df1 = {
+            'pathogen': rrr['Name of pathogen'],
+            'antibiotic': rrr.index[abioind1],
+            'sensitivity': rrr[rrr.index[abioind1]]
+        }
+        new_df1['Isolates']=25
+        megagigalist.append(new_df1)
+
+# for Staphylococcus
+for iii, rrr in df2.iterrows():
+    for abioind2 in abio_columns_2:
+        new_df2 = {
+            'pathogen': rrr['Name of pathogen'],
+            'antibiotic': rrr.index[abioind2],
+            'sensitivity': rrr[rrr.index[abioind2]]
+        }
+        new_df2['Isolates']=23
+        megagigalist.append(new_df2)
+
+# for
+for iii, rrr in df3.iterrows():
+    for abioind3 in abio_columns_3:
+        new_df3 = {
+            'pathogen': rrr['Name of pathogen'],
+            'antibiotic': rrr.index[abioind3],
+            'sensitivity': rrr[rrr.index[abioind3]]
+        }
+        new_df3['Isolates']=25
+        megagigalist.append(new_df3)
+
+
+outputloc = "outdata"
+
+poultry_data = pd.DataFrame.from_records(megagigalist)
+poultry_data = poultry_data.dropna(subset=['sensitivity']) #remove many datapoints for antibiotics not tested for each sample
 
