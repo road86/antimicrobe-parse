1656fd16bed343caeeef986098e8acf3c9953346
wali <walizmn@gmai.com>
2022-08-26 16:37:38 +0600

    tried filtering for E. Coli and set up provider-wise sorting. The original script is saved in my system under a different name. Error that was returned is in slack and github comments

--- animal_preprocess.py
+++ animal_preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 indata_summary = pd.read_csv(os.path.join("outdata","ast_data_animal_clean.csv"))
-prov='animal'
+prov=indata_summary['provider']
 loc='any'
 
 list_of_versions = list()
@@ -12,20 +12,22 @@ print(f'Processing {prov} at {loc}')
 preped_frames = []
 preped_noiso_frames = []
 
-indata_summary['specimen_category']=pd.Series("animal",index=indata_summary.index)
-prepdata3 = indata_summary.groupby(['specimen_category','pathogen','antibiotic']).sum()[['sensitive_isolates','isolates_number']].reset_index()
-prep_sum = prepdata3[['specimen_category', 'pathogen', 'antibiotic', 'sensitive_isolates','isolates_number']]
-prep_sum.columns = ['specimen_category', 'pathogen', 'antibiotic', 'sensitive_isolates', 'total']
+Ecoli = indata_summary[(indata_summary['pathogen']=="escherichia coli")]
+
+Ecoli['specimen_category']=pd.Series("Poultry",index=Ecoli.index)
+prepdata3 = Ecoli.groupby(['provider', 'specimen_category','pathogen','antibiotic']).sum()[['sensitive_isolates','isolates_number']].reset_index()
+prep_sum = prepdata3[['provider', 'specimen_category', 'pathogen', 'antibiotic', 'sensitive_isolates','isolates_number']]
+prep_sum.columns = ['provider', 'specimen_category', 'pathogen', 'antibiotic', 'sensitive_isolates', 'total']
 preped_frames.append(prep_sum)
 
-prepnoiso_sum = indata_summary[['specimen_category','pathogen','isolates_number']].drop_duplicates()
+prepnoiso_sum = Ecoli[['provider', 'specimen_category','pathogen','isolates_number']].drop_duplicates()
 
-prepnoiso_sum.columns = ['specimen_category', 'pathogen', 'number_of_isolates']
+prepnoiso_sum.columns = ['provider', 'specimen_category', 'pathogen', 'number_of_isolates']
 preped_noiso_frames.append(prepnoiso_sum)
 
 prep_both = pd.concat(preped_frames)
 
-prep_both = prep_both.groupby(['specimen_category','pathogen','antibiotic']).sum()[['sensitive_isolates','total']].reset_index()
+prep_both = prep_both.groupby(['provider', 'specimen_category','pathogen','antibiotic']).sum()[['sensitive_isolates','total']].reset_index()
 prep_both['sensitivity'] = prep_both.apply(lambda x: x['sensitive_isolates']/x['total'],axis=1)
 prep_both.to_csv(os.path.join('outdata',f'pre-processed-{prov}-{loc}.csv'))
 
@@ -33,3 +35,4 @@ prep_both_niso = pd.concat(preped_noiso_frames)
 prep_both_niso.to_csv(os.path.join('outdata',f'number-of-isolates-{prov}-{loc}.csv'))
 
 pd.DataFrame.from_records(list_of_versions).to_csv(os.path.join('outdata','list_of_versions.csv'))
+
