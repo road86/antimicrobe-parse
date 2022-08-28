60ee0403aa1ad9cfab09914d5bff7ade2ecbf636
M Kunde <mikolaj.kundegorski@gmail.com>
2022-08-29 01:55:20 +0200

    parsing CDIL data

--- parse_poultry-CDIL.py
+++ parse_poultry-CDIL.py
@@ -16,17 +16,38 @@ df.to_csv(f'{datacheck}/cdil.csv') #checking if skiprows and header give the des
 
 megagigalist = []
 
-#for each antibiotic result there is a columns
-range_abio_columns = range(6, len(df.columns))
 
-for iii, rrr in df.iterrows():
-    for abioind in range_abio_columns:
-        new_df = {
-            'amr_uuid':rrr['amr_uuid'],
-            'lab_id': rrr['Sample Submission ID/Lab ID'],
-            'specimen': rrr['Sample Name'],
-            'pathogen': rrr['pathogen'],
-            'antibiotic': rrr.index[abioind],
-            'sensitivity': rrr[rrr.index[abioind]]
-        }
+ast_data = pd.melt(df,id_vars=df.columns[0:6], value_vars=df.columns[6:-1],var_name='antibiotic', value_name='mic')
 
+
+ast_data.columns = ['amr_uuid', 'SL', 'pathogen', 'lab_id','Unnamed: 2', 'AMP', 'antibiotic', 'mic']
+ast_data = ast_data[['amr_uuid', 'pathogen', 'lab_id','antibiotic', 'mic']]
+
+#interpretation of MIC results
+
+df2 = pd.read_excel(cdil_data, sheet_name = 'data', skiprows = range(1,3,1), nrows = 3, header = [0])
+
+df2.drop('Sample Submission ID/Lab ID', inplace=True, axis=1)
+df2.drop('SL', inplace=True, axis=1)
+df2.drop('Unnamed: 21', inplace=True, axis=1)
+
+
+thresh = df2.set_index('Unnamed: 2').T
+
+ast_data['mic'] = pd.to_numeric(ast_data['mic'],errors='coerce')
+ast_data = ast_data.dropna(subset=['mic'])
+ast_data['sensitivity'] = ast_data.apply(lambda x: 'S' if x['mic'] >= thresh.loc[x['antibiotic']]['CLSI S >='] else 'R' if x['mic'] <= thresh.loc[x['antibiotic']]['CLSI R<='] else 'I', axis = 1)
+
+nisolates = len(ast_data['amr_uuid'].unique())
+nrecords = len(ast_data['amr_uuid'])
+nantiperiso = nrecords/nisolates
+provider_name = 'cdil'
+location_name = 'dhaka'
+
+ast_data['location']=pd.Series(location_name,index=ast_data.index)
+ast_data['provider']=pd.Series(provider_name,index=ast_data.index)
+
+
+print(f'Provider {provider_name} from {location_name} has {nisolates} isolates of bacteria with a total of {nrecords} antibiotic sensitivity tested, which is {nantiperiso:.2f} antibiotics tested per isolate')
+
+ast_data.to_csv(os.path.join('outdata',f'ast_poultry_cdil.csv'))
--- run_animal.sh
+++ run_animal.sh
@@ -2,6 +2,7 @@ set -e
 python parse_summary_poultry-BARC.py
 python parse_poultry-aftab.py
 python parse_poultry-nourish.py
+python parse_poultry-CDIL.py
 python compile_animal.py
 python animal_preprocess.py
 python prepare_matrix.py
