89d9d945976ac6cc438a77369c4fbb39d7da18e8
wali <walizmn@gmai.com>
2022-09-18 15:41:46 +0600

    Added overall sensitivity as a table, and sorted it in descending order. The problem that arises is that in some xlsx files, the second table gets pushed to the right by 9 columns or so

--- prepare_matrix.py
+++ prepare_matrix.py
@@ -1,4 +1,6 @@
 from ast import Lambda
+from ntpath import join
+from tkinter import N
 import pandas as pd
 import uuid
 import openpyxl
@@ -65,8 +67,8 @@ for _, ver in list_of_versions.iterrows():
             for pat, niso in df_n.iterrows():
                 new_row[(pat,'n')] = niso['number_of_isolates']
                 new_row[(pat,'%')] = mper(niso['number_of_isolates'] / total_n_iso)
-
-
+                
+            
             new_row[('overall sensitivity','%')] = '--'
             full_amb_list.append(new_row)
             tots_row = new_row
@@ -80,6 +82,7 @@ for _, ver in list_of_versions.iterrows():
                     if np.isnan(atot[pat]):
                         new_row[(pat,'n')] = 0
                         new_row[(pat,'%')] = '--'
+                        
                     else:
                         #if number of isolates tested on this antibiotic i less than some percentage  of total number of isolates we will not report sensitivity
                         new_row[(pat,'n')] = atot[pat]
@@ -100,7 +103,26 @@ for _, ver in list_of_versions.iterrows():
 
             new_columns = pd.MultiIndex.from_tuples(full_amb.columns, names=['Pathogen', 'number of isolates and sensitivity'])
             full_amb.columns = new_columns
-            full_amb.to_excel(writer,sheet_name=specimen_type)
+            
+            ######delete later
+            datacheck = 'check'
+            ## Checks
+            full_amb.to_csv(f'{datacheck}/fullamb.csv')
+            #########
+                
+            # I want to create a separate database for "overall sensitivity" from the existing one
+            # I want to remove all columns other than the name of antibiotcs and the overall sensitivity
+            # and sort the database in descending order of overall sensitivity
+            overall_sens = full_amb
+            xnew_columns = pd.MultiIndex.from_tuples(overall_sens.columns, names=['Antibiotic', 'number of isolates and sensitivity'])
+            overall_sens.columns = xnew_columns
+   
+            overall_sens = overall_sens.loc[:, ["overall sensitivity"]].sort_values(by=[("overall sensitivity", "%")], ascending=False).drop('Total number of isolates')
+            
+            full_amb.to_excel(writer,sheet_name=specimen_type, startrow=0, startcol=0)
+            
+            overall_sens.to_excel(writer,sheet_name=specimen_type, startrow=1, startcol=full_amb.shape[0]+1)
+            #the line above still needs tweaking, in case of some xlsx files this second table gets pushed 9 or more columns to the right
             # Set backgrund colors depending on cell values
             worksheet = writer.sheets[specimen_type]
 
@@ -148,4 +170,27 @@ for _, ver in list_of_versions.iterrows():
             worksheet.cell(row=2, column=1).value=None # getting rid of the line "number of isolates and sensitivity"
             worksheet.cell(row=1,column=1).alignment = Alignment(horizontal='center', vertical='center') #fixing the alignment for "Pathogen"
 
+
+############for overal sensitivity
+            worksheet.column_dimensions[worksheet.cell(row=3,column=full_amb.shape[0]+2).column_letter].width = 40
+            for ccc in range(2,len(overall_sens.columns)):
+                if ccc%2:
+                    worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
+                else:
+                    worksheet.column_dimensions[worksheet.cell(row=3,column=ccc).column_letter].width = 5
+                worksheet.cell(row=3,column=ccc).alignment = Alignment(horizontal='center', vertical='center')
+                for rrr in range(heading_size+1,heading_size+1+len(overall_sens)):
+                    worksheet.cell(row=rrr,column=ccc).alignment = Alignment(vertical='center')
+                    worksheet.cell(row=rrr,column=ccc).alignment = Alignment(horizontal='center')
+
             
+
+
+            #for rrr in range(heading_size+2,heading_size+1+len(overall_sens)):
+             #   for ccc in list(range(2,len(overall_sens.columns)+1,2))+[len(overall_sens.columns)+1]:
+              #      value_str = worksheet.cell(row=rrr,column=ccc).value
+               #     if value_str == '--':
+                #        worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('ededed'))
+                 #   else:
+                  #      value = int(value_str.rstrip('%'))
+                   #     worksheet.cell(row=rrr,column=ccc).fill = PatternFill("solid", start_color=('d62d20' if value < 50 else 'ed6f00' if value < 75 else 'eee600' if value < 90 else 'a0e040'))
